d825bd9b9a126b428b8db75971fd829275077b7d
Omondi-ai <odhiamboderrick01@gmail.com>
2022-10-13 05:45:29 +0000

    square3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,6 +6,7 @@
 
 class Square:
     """A class that represents a square"""
+
     def __init__(self, size=0):
         """Initializing this square class
         Args:
@@ -19,6 +20,7 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         """
         Calculate area of the square
