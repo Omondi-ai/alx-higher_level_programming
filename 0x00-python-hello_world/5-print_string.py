a76e8c9b2c8f6408c6521b22934b3949f92ec7be
Omondi-ai <odhiamboderrick01@gmail.com>
2023-08-09 00:24:12 -0700

    5print

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str*3 +"\n" + str[0:9])
+print(str*3 +"\n" + str[0:9#!/usr/bin/python3
+str = "Holberton School"
+print(3 * str)
+print(str[:9])])
